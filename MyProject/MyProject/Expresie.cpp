0d94d4a818d421f21469a190d49a3f0257f3c4de
Claudia Telespan <claudiatelespan@gmail.com>
2023-11-30 23:23:37 +0200

    implementare initiala clasa Expresie: atribute, getteri, setteri, constructori

---
+++ MyProject/MyProject/Expresie.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+class Expresie {
+private:
+	char* expresie=nullptr;
+	double* rezultate = nullptr;
+public:
+
+	char* getExpresie() {
+		char* copie = new char[strlen(expresie) + 1];
+		strcpy_s(copie, strlen(expresie) + 1, expresie);
+		return copie;
+	}
+
+	void setExpresie(const char* expr) {
+		if (strlen(expr) < 3)
+			throw exception("expresie vida");
+		if (expresie != nullptr)
+			delete[] expresie;
+		expresie = new char[strlen(expr) + 1];
+		strcpy_s(expresie, strlen(expr) + 1, expr);
+	}
+
+	Expresie() {
+		this->setExpresie("0");
+	}
+
+	Expresie(char* expr) {
+		this->setExpresie(expr);
+	}
+
+
+};
--- MyProject/MyProject/Source.cpp
+++ MyProject/MyProject/Source.cpp
@@ -2,5 +2,5 @@
 using namespace std;
 
 int main() {
-	cout << endl << "hello world!";
+
 }
