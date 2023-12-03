7c666b0fabb265d286da810f9ae4f798a04c1adb
Claudia Telespan <claudiatelespan@gmail.com>
2023-12-03 17:57:04 +0200

    supraincarcari operatori clasa Operator

--- MyProject/MyProject/Operatori.cpp
+++ MyProject/MyProject/Operatori.cpp
@@ -3,46 +3,46 @@
 #include <cmath>
 using namespace std;
 
-struct Stack {
-    double* elements;
-    int size;
-    int capacity;
-
-    Stack(int capacity) {
-        this->capacity = capacity;
-        this->size = 0;
-        this->elements = new double[capacity];
-    }
-
-    ~Stack() {
-        delete[] elements;
-    }
-
-    void push(double value) {
-        if (size < capacity) {
-            elements[size++] = value;
-        }
-    }
-
-    double pop() {
-        if (size > 0) {
-            return elements[--size];
-        }
-        throw exception("stiva este goala");
-    }
-
-    double top() const {
-        if (size > 0) {
-            return elements[size - 1];
-        }
-        throw exception("stiva este goala");
-    }
-
-    bool isEmpty() const {
-        return size == 0;
-    }
-
-};
+//struct Stack {
+//    double* elements;
+//    int size;
+//    int capacity;
+//
+//    Stack(int capacity) {
+//        this->capacity = capacity;
+//        this->size = 0;
+//        this->elements = new double[capacity];
+//    }
+//
+//    ~Stack() {
+//        delete[] elements;
+//    }
+//
+//    void push(double value) {
+//        if (size < capacity) {
+//            elements[size++] = value;
+//        }
+//    }
+//
+//    double pop() {
+//        if (size > 0) {
+//            return elements[--size];
+//        }
+//        throw exception("stiva este goala");
+//    }
+//
+//    double top() const {
+//        if (size > 0) {
+//            return elements[size - 1];
+//        }
+//        throw exception("stiva este goala");
+//    }
+//
+//    bool isEmpty() const {
+//        return size == 0;
+//    }
+//
+//};
 
 class Operator {
 
@@ -67,14 +67,54 @@ public:
         return (token == '+' || token == '-' || token == '*' || token == '/' || token == '^' || token == '#');
     }
 
-    int precedentaOperator(char op) const {
+    int getPrecedenta() const {
         if (op == '+' || op == '-') return 1;
         if (op == '*' || op == '/') return 2;
         if (op == '^' || op == '#') return 3;
         return 0;
     }
 
-    void aplicareOperator(Stack values, char op) {
+    bool isLeftAssociative() const {
+        return(op == '+' || op == '-' || op == '*' || op == '/');
+    }
+
+    bool isOpenParenthesis() const {
+        return (op == '(');
+    }
+
+    bool isCloseParenthesis() const {
+        return (op == ')');
+    }
+
+    bool isParenthesis() const {
+        return(isOpenParenthesis() || isCloseParenthesis());
+    }
+
+    bool operator==(const Operator& other) const {
+        return this->getPrecedenta() == other.getPrecedenta();
+    }
+
+    bool operator!=(const Operator& other) const {
+        return this->getPrecedenta() != other.getPrecedenta();
+    }
+
+    bool operator<(const Operator& other) const {
+        return this->getPrecedenta() < other.getPrecedenta();
+    }
+
+    bool operator>(const Operator& other) const {
+        return this->getPrecedenta() > other.getPrecedenta();
+    }
+
+    bool operator<=(const Operator& other) const {
+        return this->getPrecedenta() <= other.getPrecedenta();
+    }
+
+    bool operator>=(const Operator& other) const {
+        return this->getPrecedenta() >= other.getPrecedenta();
+    }
+
+    /*void aplicareOperator(Stack values, char op) {
         double operand2 = values.top(); values.pop();
         double operand1 = values.top(); values.pop();
 
@@ -92,6 +132,6 @@ public:
             values.push(operand1 / operand2);
             break;
         }
-    }
+    }*/
 };
 
