ed2a9460a121a2e36dc91eee7be1e25620d05a27
Claudia Telespan <claudiatelespan@gmail.com>
2024-01-13 01:08:48 +0200

    primire ecuatia ca param in linia comanda

--- MyProject/MyProject/main.cpp
+++ MyProject/MyProject/main.cpp
@@ -2,10 +2,18 @@
 #include "Expresie.h"
 using namespace std;
 
-int main() {
-
-	Expresie e;
-	e.runCalculator();
+int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        char* ecuatie = argv[1];
+        Expresie ex(ecuatie);
+        ex.evaluateExpresie();
+        cout << ex;
+    }
+    else {
+        
+        Expresie e;
+        e.runCalculator();
+    }
 
 	return 0;
 }
